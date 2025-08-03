39766171bcc6c48ef56fc37faf78fac55312e61d
@devyoudini <inguajohnlee31@gmail.com>
2025-08-04 05:25:04 +0800

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useLenis } from "lenis/react";
 import { useEffect, useState } from "react";
 
 function Header() {
@@ -17,10 +18,19 @@ function Header() {
     };
   }, []);
 
+  const lenis = useLenis();
+
   return (
-    <header className={isScrolled ? "drop-shadow-lg" : "drop-shadow-none"}>
+    <header
+      className={` ${isScrolled ? "drop-shadow-lg" : "drop-shadow-none"}`}
+    >
       <div className="flex w-full max-w-[900px] items-center justify-between place-self-center px-5 py-4 md:px-8">
-        <button className="text-accent text-2xl font-bold duration-500 hover:scale-115 hover:rotate-y-360">
+        <button
+          className="text-accent drop-shadow-accent text-2xl font-bold duration-500 hover:scale-115 active:scale-90 active:opacity-50"
+          onClick={() => {
+            lenis?.scrollTo(0);
+          }}
+        >
           JL
         </button>
       </div>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,6 +2,7 @@ import profile from "../assets/img/img.png";
 import CV from "../assets/cv/Ingua, John Lee CV.pdf";
 import { DownloadIcon, HandPointingDownIcon, InfoIcon } from "../assets/icons";
 import { useLenis } from "lenis/react";
+import { useEffect, useState } from "react";
 
 function Profile() {
   const lenis = useLenis();
@@ -14,14 +15,7 @@ function Profile() {
       <span className="mt-3 mb-20 font-extralight">
         Web Developer / Mobile App Developer
       </span>
-      <div className="bg-dark-secondary group/profile h-[min(70vw,400px)] w-[min(70vw,400px)] overflow-hidden rounded-full md:h-[400px] md:w-[400px]">
-        <img
-          src={profile}
-          alt="picture of John Lee"
-          className="h-full w-full object-cover duration-500 group-hover/profile:translate-x-[-1.5rem] group-hover/profile:translate-y-[3.5rem] group-hover/profile:scale-150 focus:scale-150"
-          id="profile"
-        />
-      </div>
+      <ImageLoader src={profile} alt="Picture of John Lee" />
       <div className="mt-15 flex gap-10">
         <a
           href={CV}
@@ -46,6 +40,51 @@ function Profile() {
         Explore my work below
         <HandPointingDownIcon className="animate-bounce" />
       </a>
+    </div>
+  );
+}
+
+function ImageLoader({ src, alt }: { src: string; alt: string }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+
+    const handleImageLoad = () => {
+      setImageLoaded(true);
+    };
+
+    img.addEventListener("load", handleImageLoad);
+
+    return () => {
+      img.removeEventListener("load", handleImageLoad);
+    };
+  }, [src]);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    if (imageLoaded) {
+      timeoutId = setTimeout(() => {
+        setShowImage(true);
+      }, 500);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [imageLoaded]);
+
+  return (
+    <div
+      className={`bg-dark-secondary group/profile h-[min(70vw,400px)] w-[min(70vw,400px)] overflow-hidden rounded-full md:h-[400px] md:w-[400px] ${showImage ? "animate-none" : "animate-pulse"}`}
+    >
+      <img
+        src={src}
+        alt={alt}
+        className={`h-full w-full object-cover duration-500 group-hover/profile:translate-x-[-1.5rem] group-hover/profile:translate-y-[3.5rem] group-hover/profile:scale-150 focus:scale-150 ${showImage ? "opacity-100" : "opacity-0"}`}
+        id="profile"
+      />
     </div>
   );
 }
